702705f20c438255ba31d133ca6f49ecb9fe655b
Manuel Montoya <manuelm1209@hotmail.com>
2026-05-20 17:31:51 -0500

    Add new images to home cards

--- apps/app/src/lib/visual-assets.ts
+++ apps/app/src/lib/visual-assets.ts
@@ -1,15 +1,13 @@
 export const visualAssets = {
-  cyclistHero:
-    "https://images.unsplash.com/photo-1695808403945-2eae5f50b486?auto=format&fit=crop&fm=jpg&ixlib=rb-4.1.0&q=72&w=1800",
-  aeroTrack:
-    "https://images.unsplash.com/photo-1517482359597-b3f5ba0d52ce?auto=format&fit=crop&fm=jpg&ixlib=rb-4.1.0&q=72&w=1600",
+  cyclistHero: require("../../assets/home/image-home-athmira-bike-fit.jpg"),
+  aeroTrack: require("../../assets/home/image-home-athmira-aero.jpg"),
   nutritionPlan: require("../../assets/home/image-home-athmira-nutrition.jpg"),
   tireGauge: require("../../assets/home/image-home-athmira-tire-pressure.jpg")
 } as const;
 
 export const visualAssetCredits = {
-  cyclistHero: "Max Fedyshyn / Unsplash",
-  aeroTrack: "dylan nolte / Unsplash",
+  cyclistHero: "athmira",
+  aeroTrack: "athmira",
   nutritionPlan: "athmira",
   tireGauge: "athmira"
 } as const;
